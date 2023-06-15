50e195fdc2e511f28cb7079d8935d72b81f30796
Nafees Hassan <nafeeshassan16@gmail.com>
2023-06-15 23:09:41 +0500

    Minor changes

--- src/consts.h
+++ src/consts.h
@@ -1,22 +1,17 @@
 #ifndef CONSTS_H
 #define CONSTS_H
 
-/// install path for windows 32bit
-#define INSTALL_DIR_WIN32 "C:\\plutonium\\modules\\"
-
-/// install path for windows 64bit
-#define INSTALL_DIR_WIN64 INSTALL_DIR_WIN32
-
-/// install path for linux
-#define INSTALL_DIR_LINUX "/opt/plutonium/modules/"
-
 /// repo URL
 #define REPO_URL "http://https://plutonium-lang.000webhostapp.com/fiza/"
 
 #ifdef __linux__
 #define PLATFORM_STR "Linux"
-#else
+#define INSTALL_DIR "/opt/plutonium/modules/"
+#define PKG_EXT ".so"
+#elif _WIN32 || _WIN64
 #define PLATFORM_STR "Windows"
+#define INSTALL_DIR "C:\\plutonium\\modules\\"
+#define PKG_EXT ".dll"
 #endif
 
 #endif
