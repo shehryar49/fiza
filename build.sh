147a5f206a3fe44159f6cdcc2149d4aaa937a57e
Nafees Hassan <nafeeshassan16@gmail.com>
2023-06-15 19:05:04 +0500

    Added optimisation flags for release build

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $# = 0 ]; then
 	echo "building release config";
-	g++ src/*.h src/*.cpp -o fiza;
+	g++ src/*.h src/*.cpp -s -O3 -o fiza;
 elif [ $1 = "debug" ]; then
 	echo "building debug config";
 	g++ src/*.h src/*.cpp -g -o fiza;
