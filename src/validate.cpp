a34217252f95fdc6d2a3e6aec820a059850a8505
Nafees Hassan <nafeeshassan16@gmail.com>
2023-06-15 15:57:03 +0500

    added function for package name validation

---
+++ src/validate.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include "validate.h"
+
+bool packageNameIsValid(const std::string name){
+	if (name.size() == 0)
+		return false;
+	// first character must be alphabet
+	char ch = name[0];
+	if ((ch < 'a' || ch > 'z') && (ch < 'A' || ch > 'Z'))
+		return false;
+	// now rest of characters can should be alphabets and/or digits
+	for (int i = 1; i < name.size(); i ++){
+		ch = name[i];
+		if ((ch < 'a' || ch > 'z') &&
+				(ch < 'A' || ch > 'Z') &&
+				(ch < '0' || ch > 9))
+			return false;
+	}
+	return true;
+}
---
+++ src/validate.h
@@ -0,0 +1,9 @@
+#ifndef VALIDATE_H
+#define VALIDATE_H
+
+#include <string>
+
+/// Returns: true if a package name is valid
+bool packageNameIsValid(const std::string name);
+
+#endif
