e8a4cf9cd8c82d86c30e46346e6b71c5e393c758
Shahryar Ahmad <shehryarshahid49@gmail.com>
2023-06-16 00:38:46 +0500

    Add compiler flag for C++ 17

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ $# = 0 ]; then
 	echo "building release config";
-	g++ src/*.h src/*.cpp -s -O3 -o fiza -lcurl;
+	g++ src/*.h src/*.cpp -s -O3 -o fiza -lcurl -std=c++17;
 elif [ $1 = "debug" ]; then
 	echo "building debug config";
-	g++ src/*.h src/*.cpp -g -o fiza -lcurl;
+	g++ src/*.h src/*.cpp -g -o fiza -lcurl -std=c++17;
 fi
