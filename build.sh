d6ab8b6f00e5718a23cbeb7a5855e20348414c0d
Shahryar Ahmad <shehryarshahid49@gmail.com>
2023-06-15 17:13:45 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ $# = 0 ]; then
 	echo "building release config";
-	g++ src/*.h src/*.cpp -s -O3 -o fiza;
+	g++ src/*.h src/*.cpp -s -O3 -o fiza -lcurl;
 elif [ $1 = "debug" ]; then
 	echo "building debug config";
-	g++ src/*.h src/*.cpp -g -o fiza;
+	g++ src/*.h src/*.cpp -g -o fiza -lcurl;
 fi
