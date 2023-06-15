ba7fa3abe3d37401273c801fd02807678a6659e6
Nafees Hassan <nafeeshassan16@gmail.com>
2023-06-15 15:56:38 +0500

    Added constants for URLs and install paths

---
+++ src/consts.h
@@ -0,0 +1,16 @@
+#ifndef CONSTS_H
+#define CONSTS_H
+
+/// install path for windows 32bit
+#define INSTALL_DIR_WIN32 "C:\\plutonium\\modules\\"
+
+/// install path for windows 64bit
+#define INSTALL_DIR_WIN64 INSTALL_DIR_WIN32
+
+/// install path for linux
+#define INSTALL_DIR_LINUX "/opt/plutonium/modules/"
+
+/// repo URL
+#define REPO_URL "http://https://plutonium-lang.000webhostapp.com/fiza/"
+
+#endif
