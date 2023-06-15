ebb73adb037729ef5c4a7523cab453cafe84fbbc
Nafees Hassan <nafeeshassan16@gmail.com>
2023-06-16 00:14:33 +0500

    Implemented remove and list-installed

--- src/app.cpp
+++ src/app.cpp
@@ -9,7 +9,8 @@
 #include <string>
 #include <string.h>
 #include "consts.h"
-#include "validate.h"
+#include "utils.h"
+#include "fiza.h"
 
 #include <curl/curl.h>
 
@@ -20,7 +21,8 @@ const char* USAGE_STR =
 	"\tfiza <command> <package>\n"
 	"\tvalid commands are:\n"
 	"\tinstall\t\tInstall a package\n"
-	"\tremove\t\tRemove a package\n";
+	"\tremove\t\tRemove a package\n"
+	"\tlist-installed\tList all installed packages\n";
 using namespace std;
 
 static size_t write_data(void* ptr, size_t size, size_t nmemb, void* stream)
@@ -30,38 +32,38 @@ static size_t write_data(void* ptr, size_t size, size_t nmemb, void* stream)
 }
 
 int main(int argc, char** argv){
-	if (argc != 3){
+	if (argc < 2){
 		std::cerr << USAGE_STR;
 		exit(1);
 	}
-	std::string packageName = argv[2],
-		command = argv[1];
-
-	if (!packageNameIsValid(packageName)){
-		std::cerr << "Package name seems to be invalid.\n";
-		exit(1);
+	std::string command = argv[1], packageName;
+	if (command == "install" || command == "remove"){
+		packageName = argv[2];
+		if (!packageNameIsValid(packageName)){
+			std::cerr << "Package name seems to be invalid.\n";
+			exit(1);
+		}
 	}
 
-	if (command == "install")
-	{
+	if (command == "install"){
 		string url = REPO_URL;
 		string outputPath;
-		#ifdef _WIN64
-        	url += "win64/" + packageName + ".dll";
-			outputPath = "C:\\plutonium\\modules\\" + packageName + ".dll";
-		#elif defined _WIN32
-     		url += "win32/" + packageName + ".dll";
-			outputPath = "C:\\plutonium\\modules\\" + packageName + ".dll";
-        #else
-     		url += "linux/" + packageName + ".tar.gz";
-			outputPath = "tmp.tar.gz";
-		#endif
+#ifdef _WIN64
+		url += "win64/" + packageName + ".dll";
+		outputPath = "C:\\plutonium\\modules\\" + packageName + ".dll";
+#elif defined _WIN32
+		url += "win32/" + packageName + ".dll";
+		outputPath = "C:\\plutonium\\modules\\" + packageName + ".dll";
+#else
+		url += "linux/" + packageName + ".tar.gz";
+		outputPath = "tmp.tar.gz";
+#endif
 		FILE* fp = fopen(outputPath.c_str(), "wb");
-		if (!fp)
-		{
+		if (!fp){
 			perror("fopen: ");
 			return 1;
 		}
+
 		curl_global_init(CURL_GLOBAL_ALL);
 		CURL* curl = curl_easy_init();
 		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
@@ -69,32 +71,44 @@ int main(int argc, char** argv){
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
 		CURLcode res = curl_easy_perform(curl);
-		if (res != CURLE_OK)
-		{
+		if (res != CURLE_OK){
 			printf("Installation failed: %s\n", curl_easy_strerror(res));
 			return 1;
 		}
+
 		long code = 0;
 		curl_easy_getinfo(curl,CURLINFO_HTTP_CODE, &code);
-		if (code != 200)
-		{
+		if (code != 200){
 			printf("Installation failed: HTTP Code %d\n", code);
 			return 1;
 		}
 		fclose(fp);
 		curl_global_cleanup();
-		#ifdef __linux__
-		  //TODO
-		#endif
+#ifdef __linux__
+		//TODO
+#endif
 		printf("Installation completed!");
 	}
-	else if (command == "remove")
-	{
-		// TODO remove
 
-	}
-	else
-	{
+	else if (command == "remove"){
+		if (!packageIsInstalled(packageName)){
+			std::cerr << packageName + " is not installed. Cannot remove\n";
+			exit(1);
+		}
+
+		if (packageRemove(packageName)){
+			std::cout << packageName << " was removed\n";
+		}else{
+			std::cerr << packageName << " was not removed\n";
+			exit(1);
+		}
+
+	}else if (command == "list-installed"){
+		for (auto &name : packageListInstalled()){
+			std::cout << name << "\n";
+		}
+
+	}else{
 		std::cerr << USAGE_STR;
 		exit(1);
 	}
